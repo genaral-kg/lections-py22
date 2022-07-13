78c456b869294d525bfc01e1dbab335b80bc97e2
general-kg <kutmanvip01@gmail.com>
2022-07-13 13:39:09 +0600

    added instr

--- gitlessons/commands.py
+++ gitlessons/commands.py
@@ -56,6 +56,20 @@ Changes to be committed:
      
      
      
+1. git init 
+2. git branch -M main (переимменовывам главную ветку с master на main)
+3. git add .
+4. git commit -m 'comment' (все добавлено в локальный репо)
+5. git remote add origin <url>
+6. git push origin main
+
+////////////////////
+
+
+git add .
+git commit -m 'comment'
+git push origin main
+     
      
      
      
